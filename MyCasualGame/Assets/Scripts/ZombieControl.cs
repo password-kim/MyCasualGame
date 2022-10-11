055c8235de2df006215005a20d20dd70629e92f5
password-kim <galaxykim0611@gmail.com>
2022-10-11 18:48:36 +0900

    05. ZombieControl script 작성.
    Zombie가 Player를 향해 움직이고 shuriken과 player와 충돌했을때 콘솔창에 로그로 기록하는 script를 작성.

--- MyCasualGame/Assets/Scripts/ZombieControl.cs
+++ MyCasualGame/Assets/Scripts/ZombieControl.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class ZombieControl : MonoBehaviour
 {
+    public float moveSpeed = 4.0f;
+
+    public GameObject target = null; // player.
+
+    private SpriteRenderer spriteRenderer = null;
+
+    private Transform myTransform;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.FindWithTag("Player");
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        myTransform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (target == null)
+            return;
+
+        Vector3 direction = target.transform.position - myTransform.position;
+        direction = direction.normalized;
+
+        if(direction.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if(direction.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+
+        myTransform.Translate(direction * moveSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            Debug.Log($"zombie : player Hit");
+        }
+
+        if (collision.gameObject.tag.Equals("Projectile"))
+        {
+            Debug.Log($"zombie : projectile Hit = {collision.gameObject.name}");
+        }
     }
 }
